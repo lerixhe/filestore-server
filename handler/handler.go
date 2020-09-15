74c6d4a19a7c4100f47d518c8112e0c7b1f2455b
mskj-chenting <chenting@mskj.com>
2020-09-15 22:56:36 +0800

    add file meta

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"filstore-server/meta"
+	"filstore-server/util"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 // UploadHandler 区别GET和POST，分别展示页面和接收文件
@@ -25,20 +28,29 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "./tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
 		// 创建新文件句柄
-		newFile, err := os.Create("./tmp/" + head.Filename)
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("failed to create new file %s,err:%v\n", head.Filename, err)
 			return
 		}
 		defer newFile.Close()
 		// 文件流写入新文件
-		len, err := io.Copy(newFile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("failed to save data into file,err:%v\n", err)
 			return
 		}
-		fmt.Printf("upload file succeed!,total %.2f KB\n", float64(len)/1024)
+		// 重置文件游标
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+		fmt.Printf("upload file succeed!,total %.2f KB\n", float64(fileMeta.FileSize)/1024)
 		// 跳转结果页
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
