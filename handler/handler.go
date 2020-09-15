c1996209d60241ffea29430975208e9441d29d01
mskj-chenting <chenting@mskj.com>
2020-09-15 22:12:13 +0800

    finish upload file

--- handler/handler.go
+++ handler/handler.go
@@ -21,24 +21,31 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		// 接收文件流
 		file, head, err := r.FormFile("file")
 		if err != nil {
-			fmt.Printf("failed to get data,err:%s", err)
+			fmt.Printf("failed to get data,err:%v\n", err)
 			return
 		}
 		defer file.Close()
 		// 创建新文件句柄
-		newFile, err := os.Create("/tmp/" + head.Filename)
+		newFile, err := os.Create("./tmp/" + head.Filename)
 		if err != nil {
-			fmt.Printf("failed to create new file %s,err:%s", head.Filename, err)
+			fmt.Printf("failed to create new file %s,err:%v\n", head.Filename, err)
 			return
 		}
 		defer newFile.Close()
 		// 文件流写入新文件
 		len, err := io.Copy(newFile, file)
 		if err != nil {
-			fmt.Printf("failed to save data into file,err:%s", err)
+			fmt.Printf("failed to save data into file,err:%v\n", err)
 			return
 		}
-		fmt.Printf("upload file succeed!,total %d Byte", len)
+		fmt.Printf("upload file succeed!,total %.2f KB\n", float64(len)/1024)
+		// 跳转结果页
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 
+}
+
+// UploadSucHandler 上传完成
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
 }
