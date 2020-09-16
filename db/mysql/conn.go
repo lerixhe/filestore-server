3d999026822ba1aa5e2c5dbd4ac7ff375967fa3a
何柳 <wb-hl653442@alibaba-inc.com>
2020-09-16 17:01:43 +0800

    fix db panic

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -12,9 +12,15 @@ import (
 var db *sql.DB
 
 func init() {
-	db, _ := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	var err error
+	// 注意一定不要写冒号
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	if err != nil {
+		fmt.Printf("Failed to open mysql,err:%v", err)
+		os.Exit(1)
+	}
 	db.SetMaxOpenConns(1000)
-	err := db.Ping()
+	err = db.Ping()
 	if err != nil {
 		fmt.Printf("Failed to connect to mysql,err:%v", err)
 		os.Exit(1)
